b7a4d8f49cdf313159049b046874dbc311173da5
hector-sven <hector_sven@hotmail.com>
2016-10-08 23:12:15 -0500

    Script to plot dashboard (plot 4)

--- plot4.R
+++ plot4.R
@@ -8,15 +8,19 @@ t <- t[ ,!(names(t) %in% c("Date","Time"))] ## Remove Date and Time column
 t <- cbind(dateTime, t) ## Add DateTime column
 t$dateTime <- as.POSIXct(dateTime) ## Format dateTime Column
 
-## Create plot3
+## Create plot4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(
     t,{
+        hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)")
+        plot(Global_active_power~dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
         plot(Sub_metering_1~dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
         lines(Sub_metering_2~dateTime,col='Red')
         lines(Sub_metering_3~dateTime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        ##plot(Voltage~dateTime, type="l", ylab="Voltage (volt)", xlab="")        
+        ##plot(Global_reactive_power~dateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
     }
 )
-legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-dev.copy(png,"plot3.png", width=480, height=480) ## Save file
+dev.copy(png,"plot4.png", width=480, height=480) ## Save file
 dev.off() ##close device

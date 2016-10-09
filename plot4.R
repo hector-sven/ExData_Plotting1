f9e996c08dbbb9b7be0fbfd2106c728f25d0cbe2
hector-sven <hector_sven@hotmail.com>
2016-10-09 14:09:49 -0500

    Fix on plot 4, replace histogram with correct plot

--- plot4.R
+++ plot4.R
@@ -12,14 +12,14 @@ t$dateTime <- as.POSIXct(dateTime) ## Format dateTime Column
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(
     t,{
-        hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)")
+        ##hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)")
         plot(Global_active_power~dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
         plot(Sub_metering_1~dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
         lines(Sub_metering_2~dateTime,col='Red')
         lines(Sub_metering_3~dateTime,col='Blue')
         legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-        ##plot(Voltage~dateTime, type="l", ylab="Voltage (volt)", xlab="")        
-        ##plot(Global_reactive_power~dateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
+        plot(Voltage~dateTime, type="l", ylab="Voltage (volt)", xlab="")        
+        plot(Global_reactive_power~dateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
     }
 )
 dev.copy(png,"plot4.png", width=480, height=480) ## Save file
